7b973c549c6a54fa8080e149e8036c7e7fb1a263
zakariyaehaourir <zikohaourir@gmail.com>
2026-03-12 03:38:11 +0100

    Verify cridentials in database

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,31 @@
-﻿namespace Authentication_Service.Services
+﻿using Authentication_Service.Models;
+using Authentication_Service.Records;
+using Authentication_Service.Repositories;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authentication_Service.Services
 {
     public class UserService :IUserService
     {
+        private readonly IAuthRepository _repo;
+
+        public UserService(IAuthRepository authRepository)
+        {
+            this._repo = authRepository;
+        }
+        public async Task<LoginResult> AuthenticateUser(string email, string password)
+        {
+            var user = await this._repo.GetUserByEmail(email);
+            //User doeas not match any record in the database
+            if (user is null)
+                return new LoginResult(false, string.Empty);
+            var hasher =new PasswordHasher<Users>();
+            PasswordVerificationResult result= hasher.VerifyHashedPassword(user, user.PasswordHash, password);
+            //Password does not match
+            if (result.Equals(PasswordVerificationResult.Failed))
+                return new LoginResult(false, string.Empty);
+            //All is good generate token and return :)
+            return null;
+        }
     }
 }
